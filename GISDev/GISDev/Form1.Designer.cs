ddafc452d31fc20db82e315cb7d5a2fd1622f2fd
cher9lie <30500666+cher9lie@users.noreply.github.com>
2025-10-29 18:19:16 +0800

    第二部分：编写代码添加数据和控制地图

--- GISDev/GISDev/Form1.Designer.cs
+++ GISDev/GISDev/Form1.Designer.cs
@@ -30,31 +30,28 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
-            this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
             this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
             this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tlbOpen = new System.Windows.Forms.ToolStripButton();
+            this.tlbGlobal = new System.Windows.Forms.ToolStripButton();
+            this.tlbZoomIn = new System.Windows.Forms.ToolStripButton();
+            this.tlbZoomOut = new System.Windows.Forms.ToolStripButton();
+            this.tlbPan = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // axLicenseControl1
             // 
             this.axLicenseControl1.Enabled = true;
-            this.axLicenseControl1.Location = new System.Drawing.Point(13, 13);
+            this.axLicenseControl1.Location = new System.Drawing.Point(13, 28);
             this.axLicenseControl1.Name = "axLicenseControl1";
             this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
             this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
             this.axLicenseControl1.TabIndex = 0;
-            // 
-            // axToolbarControl1
-            // 
-            this.axToolbarControl1.Location = new System.Drawing.Point(67, 17);
-            this.axToolbarControl1.Name = "axToolbarControl1";
-            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
-            this.axToolbarControl1.Size = new System.Drawing.Size(793, 28);
-            this.axToolbarControl1.TabIndex = 1;
             // 
             // axTOCControl1
             // 
@@ -71,33 +68,110 @@
             this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
             this.axMapControl1.Size = new System.Drawing.Size(615, 654);
             this.axMapControl1.TabIndex = 3;
+            this.axMapControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlbOpen,
+            this.tlbGlobal,
+            this.tlbZoomIn,
+            this.tlbZoomOut,
+            this.tlbPan});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(894, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tlbOpen
+            // 
+            this.tlbOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlbOpen.Image = ((System.Drawing.Image)(resources.GetObject("tlbOpen.Image")));
+            this.tlbOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlbOpen.Name = "tlbOpen";
+            this.tlbOpen.Size = new System.Drawing.Size(23, 22);
+            this.tlbOpen.Text = "tlbOpen";
+            this.tlbOpen.ToolTipText = "打开图层";
+            this.tlbOpen.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // tlbGlobal
+            // 
+            this.tlbGlobal.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlbGlobal.Image = ((System.Drawing.Image)(resources.GetObject("tlbGlobal.Image")));
+            this.tlbGlobal.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlbGlobal.Name = "tlbGlobal";
+            this.tlbGlobal.Size = new System.Drawing.Size(23, 22);
+            this.tlbGlobal.Text = "tlbGlobal";
+            this.tlbGlobal.ToolTipText = "全屏";
+            this.tlbGlobal.Click += new System.EventHandler(this.tlbGloble_Click);
+            // 
+            // tlbZoomIn
+            // 
+            this.tlbZoomIn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlbZoomIn.Image = ((System.Drawing.Image)(resources.GetObject("tlbZoomIn.Image")));
+            this.tlbZoomIn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlbZoomIn.Name = "tlbZoomIn";
+            this.tlbZoomIn.Size = new System.Drawing.Size(23, 22);
+            this.tlbZoomIn.Text = "tlbZoomIn";
+            this.tlbZoomIn.ToolTipText = "放大";
+            this.tlbZoomIn.Click += new System.EventHandler(this.tlbZoomIn_Click);
+            // 
+            // tlbZoomOut
+            // 
+            this.tlbZoomOut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlbZoomOut.Image = ((System.Drawing.Image)(resources.GetObject("tlbZoomOut.Image")));
+            this.tlbZoomOut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlbZoomOut.Name = "tlbZoomOut";
+            this.tlbZoomOut.Size = new System.Drawing.Size(23, 22);
+            this.tlbZoomOut.Text = "tlbZoomOut";
+            this.tlbZoomOut.ToolTipText = "缩小";
+            this.tlbZoomOut.Click += new System.EventHandler(this.tlbZoomOut_Click);
+            // 
+            // tlbPan
+            // 
+            this.tlbPan.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlbPan.Image = ((System.Drawing.Image)(resources.GetObject("tlbPan.Image")));
+            this.tlbPan.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlbPan.Name = "tlbPan";
+            this.tlbPan.Size = new System.Drawing.Size(23, 22);
+            this.tlbPan.Text = "tlbPan";
+            this.tlbPan.ToolTipText = "漫游";
+            this.tlbPan.Click += new System.EventHandler(this.tlbPan_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(894, 761);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.axMapControl1);
             this.Controls.Add(this.axTOCControl1);
-            this.Controls.Add(this.axToolbarControl1);
             this.Controls.Add(this.axLicenseControl1);
             this.Name = "Form1";
             this.Text = "MapViewer";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
-        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
         private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
         private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tlbOpen;
+        private System.Windows.Forms.ToolStripButton tlbGlobal;
+        private System.Windows.Forms.ToolStripButton tlbZoomIn;
+        private System.Windows.Forms.ToolStripButton tlbZoomOut;
+        private System.Windows.Forms.ToolStripButton tlbPan;
     }
 }
 
--- GISDev/GISDev/Form1.cs
+++ GISDev/GISDev/Form1.cs
@@ -6,20 +6,91 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geometry;
 
 namespace GISDev
 {
     public partial class Form1 : Form
     {
+        string m_BasicOperationTool = "";
         public Form1()
         {
             ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.Engine);
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Title = "open map document";
+            openFileDialog.Filter = "map document (*.mxd)|*.mxd";
+            openFileDialog.ShowDialog();
+            string strFilePath = openFileDialog.FileName;
+            if (axMapControl1.CheckMxFile(strFilePath))
+            {
+                axMapControl1.MousePointer = ESRI.ArcGIS.Controls.esriControlsMousePointer.esriPointerHourglass;
+                axMapControl1.LoadMxFile(strFilePath, 0, Type.Missing);
+                axMapControl1.MousePointer = ESRI.ArcGIS.Controls.esriControlsMousePointer.esriPointerDefault;
+            }
+            else
+            {
+                MessageBox.Show(strFilePath + " is not a valid map document");
+            }
+        }
+
+        // 全屏
+        private void tlbGloble_Click(object sender, EventArgs e)
+        {
+            m_BasicOperationTool = "isZoomFull";
+            axMapControl1.Extent = axMapControl1.FullExtent; // 直接生效
+        }
+        // 放大
+        private void tlbZoomIn_Click(object sender, EventArgs e)
+        {
+            m_BasicOperationTool = "ZoomIn";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerZoomIn;
+        }
+        // 缩小
+        private void tlbZoomOut_Click(object sender, EventArgs e)
+        {
+            m_BasicOperationTool = "ZoomOut";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerZoomOut;
+        }
+        // 漫游
+        private void tlbPan_Click(object sender, EventArgs e)
+        {
+            m_BasicOperationTool = "ZoomPan";
+            axMapControl1.MousePointer = esriControlsMousePointer.esriPointerPan;
+        }
+
+        private void axMapControl1_OnMouseDown(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+            // 本方法用于在axMapControl上进行点击后才生效的功能,如放大缩小和漫游
+            switch (m_BasicOperationTool)
+            {
+                case "ZoomIn":
+                    axMapControl1.Extent = axMapControl1.TrackRectangle(); // 拉框放大
+                    break;
+                case "ZoomOut":
+                    IEnvelope objEnvelope = axMapControl1.TrackRectangle();
+                    // 扩大包络线范围以缩小显示
+                    objEnvelope.Expand(2, 2, true);
+                    axMapControl1.Extent = objEnvelope;
+                    break;
+                case "ZoomPan":
+                    axMapControl1.Pan(); // 鼠标拖动平移
+                    break;
+            }
         }
     }
 }
